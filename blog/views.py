7b89d9b4421872c0af620c76274306b08f60b353
andytosa <andy.tosa98@gmail.com>
2020-03-25 12:19:42 +0200

    Created database
    Created Post database model.

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'Corey',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 69, 2020',
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 70, 2020',
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context) 
 
